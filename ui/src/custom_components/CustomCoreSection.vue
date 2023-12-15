ead1f1a7143ffa46959bef1f7ff6d22ec3d27472
zach <zachariah.eb@protonmail.com>
2023-12-15 13:07:26 -0800

    added custom vue files

---
+++ ui/src/custom_components/CustomCoreSection.vue
@@ -0,0 +1,167 @@
+<template>
+	<section 
+    class="CoreSection"
+	ref="rootEl" 
+    :class="{ snapMode: fields.snapMode.value == 'yes' }"
+	v-on:click.capture="captureClick" 
+    >
+		<h2 v-if="fields.title.value">{{ fields.title.value }} CUSTOM SECTION</h2>
+		<div data-streamsync-container 
+		v-on:change.capture="captureChange"
+		><slot></slot></div>
+	</section>
+</template>
+
+<script lang="ts">
+
+import { FieldCategory, FieldType } from "../streamsyncTypes";
+import {
+	accentColor,
+	primaryTextColor,
+	secondaryTextColor,
+	containerBackgroundColor,
+	containerShadow,
+	separatorColor,
+	buttonColor,
+	buttonTextColor,
+	buttonShadow,
+cssClasses,
+} from "../renderer/sharedStyleFields";
+import { assembleParameterSignals } from "vega-lite/build/src/parameter";
+import { TRAIL } from "vega-lite/build/src/mark";
+
+const description =
+	"A container component that divides the layout into sections, with an optional title.";
+
+export default {
+	streamsync: {
+		name: "Section",
+		description,
+		category: "Layout",
+		allowedChildrenTypes: ["*"],
+		fields: {
+			title: {
+				name: "Title",
+				init: "Section Title",
+				desc: "Leave blank to hide.",
+				type: FieldType.Text,
+			},
+			accentColor,
+			primaryTextColor,
+			secondaryTextColor,
+			containerBackgroundColor,
+			containerShadow,
+			separatorColor,
+			buttonColor,
+			buttonTextColor,
+			buttonShadow,
+			snapMode: {
+				name: "Snap mode",
+				type: FieldType.Text,
+				options: {
+					no: "No",
+					yes: "Yes",
+				},
+				default: "no",
+				init: "no",
+				category: FieldCategory.Style,
+				desc: "Use as much space as possible without altering the size of the container.",
+			},
+			cssClasses,
+		},
+		previewField: "title",
+	},
+};
+</script>
+<script setup lang="ts">
+import { inject } from "vue";
+import injectionKeys from "../injectionKeys";
+
+const ss = inject(injectionKeys.core);
+const instancePath = inject(injectionKeys.instancePath);
+
+function getComponentId(event: Event): string {
+	const targetEl: HTMLElement = (event.target as HTMLElement).closest(
+		"[data-streamsync-id]"
+	);
+
+	const componentId = targetEl.dataset.streamsyncId;
+	return componentId
+}
+
+function getIdentifier(event: Event): string {
+	const targetEl: HTMLElement = (event.target as HTMLElement).closest(
+		"[data-streamsync-id]"
+	);
+
+	var component = ss.getComponentById(targetEl.dataset.streamsyncId)
+
+	console.log('target component: ')
+	console.log(component)
+
+	var customId = component.content["customId"]
+	var defaultId = targetEl.dataset.streamsyncId
+
+	return (customId != "") ? customId : defaultId
+}
+
+function isCorrectInputType(event: Event, expectedTypes): boolean {
+    const type = (<HTMLInputElement>event.target).nodeName	
+    return expectedTypes.includes(type)
+}
+
+function captureClick(event: Event) {
+    event.stopPropagation()
+    if (!isCorrectInputType(event, ["BUTTON"])) { return }
+
+	const customId = getIdentifier(event)
+	const customEvent = new CustomEvent("click", {
+		detail: {
+			payload: {
+				id: customId,
+			},
+		},
+	});
+	ss.forwardEvent(customEvent, instancePath, true)
+}
+
+function captureChange(event: Event) {
+	event.stopPropagation()
+    if (!isCorrectInputType(event, ["SELECT", "INPUT"])) { return }
+
+	const componentId = getIdentifier(event)
+	const inputValue = (<HTMLInputElement>event.target).value
+	const customEvent = new CustomEvent("change", {
+		detail: {
+			payload: {
+				id: componentId,
+				value: inputValue
+			},
+		},
+	});
+
+	ss.forwardEvent(customEvent, instancePath, true)
+}
+
+const fields = inject(injectionKeys.evaluatedFields);
+</script>
+
+<style scoped>
+@import "../renderer/sharedStyles.css";
+.CoreSection {
+	padding: 16px;
+	border: 1px solid var(--separatorColor);
+	border-radius: 8px;
+	box-shadow: var(--containerShadow);
+	background-color: var(--containerBackgroundColor);
+}
+
+.CoreSection.snapMode {
+	flex: 1 0 auto;
+	align-self: stretch;
+}
+
+h2 {
+	margin-bottom: 16px;
+}
+</style>
