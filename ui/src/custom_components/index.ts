05bae3fd6a8e0ebf6265e12e1c30931adb8f6e0c
zach <zachariah.eb@protonmail.com>
2024-01-17 21:33:26 -0600

    built master components

--- ui/src/custom_components/index.ts
+++ ui/src/custom_components/index.ts
@@ -3,10 +3,18 @@
 
 import BubbleMessage from './BubbleMessage.vue';
 import BubbleMessageAdvanced from './BubbleMessageAdvanced.vue';
+import CustomCoreSection from './CustomCoreSection.vue';
+import CustomCoreButton from './CustomCoreButton.vue';
+import CoreDropdownInput from './inputs/CoreDropdownInput.vue';
+import CoreTextInput from './inputs/CoreTextInput.vue';
 
 // Export an object with the ids and the templates as default
 
 export default {
     "bubblemessage": BubbleMessage,
-    "bubblemessageadvanced": BubbleMessageAdvanced
+    "bubblemessageadvanced": BubbleMessageAdvanced,
+    "customcoresection": CustomCoreSection,
+    "customcorebutton": CustomCoreButton,
+    "coredropdowninput": CoreDropdownInput,
+    "coretextinput": CoreTextInput,
 }
