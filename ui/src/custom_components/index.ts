9df58955f03a980fce1e9fbf25ea29dd098326fc
zach <zachariah.eb@protonmail.com>
2024-01-24 22:49:43 -0600

    chart WIP

--- ui/src/custom_components/index.ts
+++ ui/src/custom_components/index.ts
@@ -5,6 +5,7 @@ import CustomSection from './CustomCoreSection.vue';
 import CustomButton from './CustomCoreButton.vue';
 import CustomDropDown from './inputs/CoreDropDownInput.vue';
 import CustomTextInput from './inputs/CoreTextInput.vue';
+import CustomVegaChart from './CustomCoreVegaLiteChart.vue';
 
 // Export an object with the ids and the templates as default
 
@@ -13,4 +14,5 @@ export default {
     "custombutton": CustomButton,
     "customdropdown": CustomDropDown,
     "customtextinput": CustomTextInput,
+    "customvegachart": CustomVegaChart,
 }
